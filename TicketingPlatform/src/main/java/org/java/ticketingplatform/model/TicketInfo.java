28639ab42195de1288f81dca5abc07abb1381c53
James_ZY_L <130677389+James-Zeyu-Li@users.noreply.github.com>
2025-08-23 23:48:55 -0700

    Start migration from JDBC to JPA for better database connection framework

--- TicketingPlatform/src/main/java/org/java/ticketingplatform/model/TicketInfo.java
+++ TicketingPlatform/src/main/java/org/java/ticketingplatform/model/TicketInfo.java
@@ -1,18 +1,40 @@
 package org.java.ticketingplatform.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
+@Entity
+@Table(name = "ticket")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TicketInfo {
+	@Id
+	@Column(name = "ticket_id")
 	private String ticketId;
+
+	@Column(name = "venue_id")
 	private String venueId;
+
+	@Column(name = "event_id")
 	private String eventId;
+
+	@Column(name = "zone_id")
 	private int zoneId;
+
+	@Column(name = "col_label")
 	private String column;
+
+	@Column(name = "row_label")
 	private String row;
+	// have restrictions on changing this date
+	@Column(name = "created_on", nullable = false, updatable = false)
 	private Instant createdOn;
 }
