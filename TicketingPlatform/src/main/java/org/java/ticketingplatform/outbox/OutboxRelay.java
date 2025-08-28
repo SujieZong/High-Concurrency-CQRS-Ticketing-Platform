b0da004bff0b3d96e3b7070561b45320a380b894
James_ZY_L <130677389+James-Zeyu-Li@users.noreply.github.com>
2025-08-27 23:26:18 -0700

    Add MySQLConsumer back, add a producer Config for sending JSON data

--- TicketingPlatform/src/main/java/org/java/ticketingplatform/outbox/OutboxRelay.java
+++ TicketingPlatform/src/main/java/org/java/ticketingplatform/outbox/OutboxRelay.java
@@ -1,8 +1,10 @@
 package org.java.ticketingplatform.outbox;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.java.ticketingplatform.config.rabbitmq.RabbitFactory;
 import org.java.ticketingplatform.domain.OutboxEvent;
+import org.java.ticketingplatform.dto.MqDTO;
 import org.java.ticketingplatform.repository.OutboxEventRepository;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,13 +17,16 @@ import java.util.List;
 public class OutboxRelay {
 	private final OutboxEventRepository repo;
 	private final RabbitTemplate rabbit;
+	private final ObjectMapper objectMapper;
+
 
 	@Scheduled(fixedDelay = 500) //every 0.5 second scan
 	public void flush() {
 		List<OutboxEvent> batch = repo.findTop50BySentFalseOrderByIdAsc();
 		for (OutboxEvent e : batch) {
 			try {
-				rabbit.convertAndSend(RabbitFactory.TICKET_EXCHANGE, "ticket.created", e.getPayload());
+				MqDTO dto = objectMapper.readValue(e.getPayload(), MqDTO.class);
+				rabbit.convertAndSend(RabbitFactory.TICKET_EXCHANGE, "ticket.created", dto);
 				e.setSent(true);
 				repo.save(e);
 			} catch (Exception ex) {
