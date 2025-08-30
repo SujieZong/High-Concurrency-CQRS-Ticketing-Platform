84bfe7b87239dabc8de05b8d802e8bd958c5c868
James_ZY_L <130677389+James-Zeyu-Li@users.noreply.github.com>
2025-08-29 19:02:16 -0700

    update dependencu, dockerfile and request file

--- QueryService/src/main/java/org/java/queryservice/dto/ErrorMessage.java
+++ QueryService/src/main/java/org/java/queryservice/dto/ErrorMessage.java
@@ -2,9 +2,11 @@ package org.java.queryservice.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ErrorMessage {
 	private String messages;
 }
