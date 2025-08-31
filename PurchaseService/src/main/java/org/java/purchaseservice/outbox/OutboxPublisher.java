dc795879ecaf61ffeb6915ebabeae15258d4db32
James_ZY_L <130677389+James-Zeyu-Li@users.noreply.github.com>
2025-08-31 14:14:30 -0700

    update outbox async to outbox + Spring web Stream + Rabbit binder

---
+++ PurchaseService/src/main/java/org/java/purchaseservice/outbox/OutboxPublisher.java
@@ -0,0 +1,43 @@
+package org.java.purchaseservice.outbox;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.java.purchaseservice.domain.OutboxEvent;
+import org.java.purchaseservice.repository.OutboxEventRepository;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OutboxPublisher {
+	private final OutboxEventRepository repo;
+	private final StreamBridge streamBridge;
+//	private final ObjectMapper objectMapper;
+
+
+	@Scheduled(fixedDelay = 500) //every 0.5 second scan
+	@Transactional
+	public void flush() {
+		var batch = repo.findTop50BySentFalseOrderByIdAsc();
+
+		for (OutboxEvent e : batch) {
+			try {
+				var message = org.springframework.messaging.support.MessageBuilder
+						.withPayload(e.getPayload()) // String/JSON
+						.build();
+
+				boolean ok = streamBridge.send("ticket-out-0", message);
+				if (!ok) throw new IllegalStateException("stream send failed");
+
+				e.setSent(true);
+				e.setAttempts(e.getAttempts() + 1);
+				repo.save(e);
+			} catch (Exception ex) {
+				e.setAttempts(e.getAttempts() + 1);
+				repo.save(e);
+			}
+		}
+	}
+}
