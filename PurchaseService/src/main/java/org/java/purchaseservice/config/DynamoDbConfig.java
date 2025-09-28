b592ef4a2b42d24ae3943a0cdb1b9ad94f666261
James_ZY_L <130677389+James-Zeyu-Li@users.noreply.github.com>
2025-09-27 19:48:55 -0700

    update RabbitCombinedConsumer yaml file to include Kafka Setting, add KafkaMySQL consumer to read from binder

--- PurchaseService/src/main/java/org/java/purchaseservice/config/DynamoDbConfig.java
+++ PurchaseService/src/main/java/org/java/purchaseservice/config/DynamoDbConfig.java
@@ -2,8 +2,6 @@ package org.java.purchaseservice.config;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
