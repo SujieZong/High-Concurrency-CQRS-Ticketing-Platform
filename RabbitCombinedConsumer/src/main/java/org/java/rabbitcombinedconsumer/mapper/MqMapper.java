2fbe073d953e05cf25fde70b65dbe931403fa4f6
James_ZY_L <130677389+James-Zeyu-Li@users.noreply.github.com>
2025-09-27 18:16:05 -0700

    update Java Runtime to Version 21 from 17

--- RabbitCombinedConsumer/src/main/java/org/java/rabbitcombinedconsumer/mapper/MqMapper.java
+++ RabbitCombinedConsumer/src/main/java/org/java/rabbitcombinedconsumer/mapper/MqMapper.java
@@ -14,16 +14,6 @@ public interface MqMapper {
 
 	@Mapping(target = "status", expression = "java(mapStatus(mqDTO.getStatus()))")
 	TicketInfo toTicketInfo(MqDTO mqDTO);
-//
-//	default TicketStatus mapStatus(String s) {
-//		if (s == null) return TicketStatus.PENDING_PAYMENT;
-//		return switch (s.trim().toUpperCase()) {
-//			case "PAID"      -> TicketStatus.PAID;
-//			case "CANCELLED" -> TicketStatus.CANCELLED;
-//			case "CREATED"   -> TicketStatus.PENDING_PAYMENT;
-//			default          -> TicketStatus.PENDING_PAYMENT;
-//		};
-//	}
 
 	default TicketStatus mapStatus(TicketStatus s) {
 		return s == null ? TicketStatus.PENDING_PAYMENT : s;
