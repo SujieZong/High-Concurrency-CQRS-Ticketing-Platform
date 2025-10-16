345bcd4fa58183b38857f93d46740c906b77b286
James_ZY_L <130677389+James-Zeyu-Li@users.noreply.github.com>
2025-10-15 23:27:45 -0700

    Update Kafka Message publisher and interface according to steam publisher, replace outbox

--- PurchaseService/src/main/java/org/java/purchaseservice/publisher/KafkaStreamPublisher.java
+++ PurchaseService/src/main/java/org/java/purchaseservice/publisher/KafkaStreamPublisher.java
@@ -1,31 +1,39 @@
-package org.java.purchaseservice.outbox;
+package org.java.purchaseservice.publisher;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
+/**
+ * Send messages to Kafka through Spring Cloud Stream
+ */
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class KafkaStreamPublisher implements OutboxMessagePublisher {
+public class KafkaStreamPublisher implements MessagePublisher {
 
 	private final StreamBridge streamBridge;
+
+	// directly call Binding name from YML instead of hard code.
+	@Value("${kafka.binding.ticket-out}")
+	private String kafkaBinding;
 
 	@Override
 	public boolean kafkaPublish(String payload, String partitionKey) {
 		try {
-			log.info("【KafkaPublisher】Preparing to send message. partitionKey={}, payloadLength={}",
-					partitionKey, payload.length());
+			log.info("【KafkaPublisher】Preparing to send message. binding={}, partitionKey={}, payloadLength={}",
+					kafkaBinding, partitionKey, payload.length());
 			log.debug("【KafkaPublisher】Message payload: {}", payload);
 
 			Message<String> msg = MessageBuilder.withPayload(payload)
-					.setHeader("partitionKey", partitionKey) // 匹配 yml 中的 headers['partitionKey']
+					.setHeader("partitionKey", partitionKey)
 					.build();
 
-			boolean sent = streamBridge.send("ticket-out-0", msg);
+			boolean sent = streamBridge.send(kafkaBinding, msg);
 
 			if (sent) {
 				log.info("【KafkaPublisher】Message sent to Kafka successfully. partitionKey={}", partitionKey);
